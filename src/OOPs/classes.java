215f814add9ed50855c34b2639d3e1a8ec3903f9
AabhaAB <12aabha2136@gmail.com>
2023-05-26 11:54:43 +0530

    First Program

--- src/OOPs/classes.java
+++ src/OOPs/classes.java
@@ -1,10 +1,13 @@
 package OOPs;
 
-public class classes {
+public class classes { //The keyword class is used to declare a class named classes.
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//The keyword public indicates that the class is accessible from other classes.
+		//The main method is the entry point of the program and is required in every Java program. 
+		//It has the signature public static void main(String[] args).
+		
+		 System.out.println("Hello, World!");	
+		 //The System.out.println statement is used to print the text "Hello, World!" to the console.
 	}
-
 }
